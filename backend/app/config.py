7579f7412539292dd51037faa09c37bee0c0c578
Viktor Vedral <kvikimart07@gmail.com>
2026-09-14 22:24:05 +0200

    Include CORS_ORIGINS in configuration
    Add CORS_ORIGINS environment variable to config

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,7 +8,9 @@ class Config:
     FLASK_DB_NAME: str = os.getenv("FLASK_DB_NAME", "product")
     FLASK_DB_USER: str = os.getenv("FLASK_DB_USER", "root")
     FLASK_DB_PASSWORD: str = os.getenv("FLASK_DB_PASSWORD", "")
-
+    CORS_ORIGINS: list[str] = os.getenv(
+        "CORS_ORIGINS", "http://localhost:3001"
+    ).split(",")
     SQLALCHEMY_DATABASE_URI: str = (
         f"mysql+pymysql://{FLASK_DB_USER}:{quote_plus(FLASK_DB_PASSWORD)}"
         f"@{FLASK_DB_HOST}:{FLASK_DB_PORT}/{FLASK_DB_NAME}"
