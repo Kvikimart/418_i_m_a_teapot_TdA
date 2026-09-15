229279f44cc8dfe6d2d60d1359cbedf894364e3f
Viktor Vedral <kvikimart07@gmail.com>
2026-09-15 09:52:47 +0200

    Fix team members format in initialization

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -20,7 +20,7 @@ def create_app():
        
         if not Team.query.first():
            
-            my_team = Team(name="418 I'm a teapot", members=["Viktor Vedral", "Klára Tipplová" ,"Matyáš Baloun"]
+            my_team = Team(name="418 I'm a teapot", members="Viktor Vedral, Klára Tipplová, Matyáš Baloun")
             db.session.add(my_team)
             db.session.commit()
 
