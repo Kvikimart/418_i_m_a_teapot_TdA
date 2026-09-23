343885d9875d926dbd58753b413958a272efd013
Maty <137884935+MetyTwister@users.noreply.github.com>
2026-09-23 15:44:05 +0200

    Add migration initialization to Flask app
    Initialize migration setup for the Flask app.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,16 +11,14 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     CORS(app, resources={r"/api/*": {"origins": Config.CORS_ORIGINS}})
 
     app.register_blueprint(health_bp, url_prefix="/api/v1")
     app.register_blueprint(team_bp, url_prefix="/api/v1")
 
     with app.app_context():
-        db.create_all()
-       
         if not Team.query.first():
-           
             my_team = Team(name="418 I'm a teapot", members="Viktor Vedral, Klára Tipplová, Matyáš Baloun")
             db.session.add(my_team)
             db.session.commit()
