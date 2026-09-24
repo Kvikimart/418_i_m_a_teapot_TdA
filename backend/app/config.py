c183bd613bf427ee05447cf74af10e8c5d447ad7
Viktor Vedral <kvikimart07@gmail.com>
2026-09-24 10:21:24 +0200

    Change default database name to 'test_database'

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from urllib.parse import quote_plus
 class Config:
     FLASK_DB_HOST: str = os.getenv("FLASK_DB_HOST", "localhost")
     FLASK_DB_PORT: str = os.getenv("FLASK_DB_PORT", "3306")
-    FLASK_DB_NAME: str = os.getenv("FLASK_DB_NAME", "product")
+    FLASK_DB_NAME: str = os.getenv("FLASK_DB_NAME", "test_database")
     FLASK_DB_USER: str = os.getenv("FLASK_DB_USER", "root")
     FLASK_DB_PASSWORD: str = os.getenv("FLASK_DB_PASSWORD", "")
     CORS_ORIGINS: list[str] = os.getenv(
