ffcd057909bf5787e30308cc4791b3ce41e8eb73
Viktor Vedral <kvikimart07@gmail.com>
2026-09-25 23:19:42 +0200

    Implement get_stops route in stops_bp
    Add a new route to retrieve stops information.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -6,8 +6,14 @@ from .models import Team
 
 health_bp = Blueprint("health", __name__)
 team_bp = Blueprint("team", __name__)
+stops_bp = Blueprint("stops", __name__)
 
-
+@stops_bp.get("/stops")
+def get_stops():
+    if stops is None
+        return jsonify({"error": "No stops found"}), 404
+    return jsonify({"image url": stops.imageURL, "id": stops.id, "name": stops.name, "x": stops.x, "y": stops.y, 'tranfer': "yes" if stop.transfer else "no", 'wheel chair accessible': "yes" if stop.wheelAccessible else "no", 'shelter': "yes" if stop.shelter else "no", 'bench': "yes" if stop.bench else "no", 'display': "yes" if stop.display else "no",  'ticket machine': "yes" if stop.tikcetMachine else "no"}), 200
+                    
 @health_bp.get("/health")
 def health_check():
     return jsonify({"status": "ok"}), 200
